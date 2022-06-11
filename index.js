5c14523371593713a8c94758e7e8684baa58218f
Andrey Boberskiy <andreyboberskiy@Andreys-MacBook-Pro.local>
2022-06-11 15:53:28 +0300

    Added logs req

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const exec = require("child_process").exec;
 const config = require("./commands.json").commands;
 
 const app = express();
+app.use(express.json());
 
 const port = 999;
 
 function generateController(command) {
   return (req, res) => {
+    console.log("Req: ", req.body);
     exec(command);
 
     res.status(200).json("success");
